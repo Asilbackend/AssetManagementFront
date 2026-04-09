1ab715fff13822618491eeaeefbbfcf3ffa3178b
Asilbek <asilbackend12@gmail.com>
2026-04-09 15:34:08 +0500

    added roles director, xojalik ishlari rahbari(admin.procurement) , modified vite config to local facing

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // add host and port configuration for development server
+  server: {
+    host: '0.0.0.0',
+  }
 })
